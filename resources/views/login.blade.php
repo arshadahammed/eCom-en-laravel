4c56dcac2a720d71af5c5c3b8d274c80b03765b3
arshadahammed <arshadahammed8@gmail.com>
2022-01-23 10:50:30 +0530

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,20 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 offset-md-3">
-<form>
+<form action="login" method="POST">
   <div class="mb-3">
+    @csrf
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
 
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Login</button>
+  {{-- dfdvd  --}}
 </form>
 </div>
 </div>
